f8db3db88ba97e6059bc6ecdc9b63feca078c1b5
Gage Messner <gmessner@lion.lmu.com>
2023-03-22 18:47:52 -0700

    matrix start

---
+++ src/matrix.js
@@ -0,0 +1,101 @@
+//Matrix Library
+
+//Matrix Generator that automatically outputs an identity matrix
+function new4x4Matrix(){
+    const m = mat4.create()
+    mat4.identity(m)
+    return m
+}
+
+// 4x4 Matrix multiplier
+// need help on how to access the individual rows, cols
+function multiply4x4Matrices(m1, m2){
+    var result = []
+    if(m1.size() != m2.size()){
+        console.error();
+        return
+    }
+
+    for(let i = 0; i < m1.size(); i++){
+        for(let j = 0; j < m1[3][3]; j++){
+            result[i][j] = 0
+            for(let k = 0; k < m2[3][3]; k++){
+                result += m1[i][j] * m2[j][k]
+            }
+        }
+    }
+    console.log("your result is: ", result)
+    return result
+}
+
+
+/* Individual Matrix Implementations
+* Gage Messner: Matrix Translations
+* Kevin: 
+* Mitch: 
+* David: 
+* 
+* Group: 
+*/
+
+// Translates a given matrix by whatever numbers you want. 'a' translates the x-coord, 
+// 'b' translates the y-coord, 'c' translates the z-coord
+function translateMatrix(matrix4, a, b, c){
+    matrix4[3] += a // last entry in col. 1
+    matrix4[7] += b // last entry in col. 2
+    matrix4[11] += c // last entry in col. 3
+    return matrix4
+}
+
+
+// Rotation Matrix from class
+// CAN be refactored for our use
+
+const rotationMatrix = (angle, x, y, z) => {
+    // In production code, this function should be associated
+    // with a matrix object with associated functions.
+    const axisLength = Math.sqrt(x * x + y * y + z * z)
+    const s = Math.sin((angle * Math.PI) / 180.0)
+    const c = Math.cos((angle * Math.PI) / 180.0)
+    const oneMinusC = 1.0 - c
+  
+    // Normalize the axis vector of rotation.
+    x /= axisLength
+    y /= axisLength
+    z /= axisLength
+  
+    // Now we can calculate the other terms.
+    // "2" for "squared."
+    const x2 = x * x
+    const y2 = y * y
+    const z2 = z * z
+    const xy = x * y
+    const yz = y * z
+    const xz = x * z
+    const xs = x * s
+    const ys = y * s
+    const zs = z * s
+  
+    // GL expects its matrices in column major order.
+    return [
+      x2 * oneMinusC + c,
+      xy * oneMinusC + zs,
+      xz * oneMinusC - ys,
+      0.0,
+  
+      xy * oneMinusC - zs,
+      y2 * oneMinusC + c,
+      yz * oneMinusC + xs,
+      0.0,
+  
+      xz * oneMinusC + ys,
+      yz * oneMinusC - xs,
+      z2 * oneMinusC + c,
+      0.0,
+  
+      0.0,
+      0.0,
+      0.0,
+      1.0
+    ]
+  }
