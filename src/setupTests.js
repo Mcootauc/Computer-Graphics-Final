6e1dbf31ca19d610b58735eeb28654f0cae22a35
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-03-24 22:57:26 -0700

    added jest to package and matrix tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,22 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
+const scaleMatrix = require('./matrix');
+
+let baseMatrix = mat4.fromValues(
+    1, 0, 0, 0,  // First column
+    0, 1, 0, 0,  // Second column
+    0, 0, 1, 0,  // Third column
+    0, 0, 0, 1   // Fourth column
+);
+
+let matrixAnswer = mat4.fromValues(
+    2, 0, 0, 0,  // First column
+    0, 3, 0, 0,  // Second column
+    0, 0, 4, 0,  // Third column
+    0, 0, 0, 1   // Fourth column
+);
+
+test('checks scaleMatrix', () => {
+  expect(scaleMatrix(baseMatrix, 2, 3, 4)).toBe(matrixAnswer);
+});
