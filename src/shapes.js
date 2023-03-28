043fe9f43797d571d7608cebd60d18569cfcae20
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-03-27 21:49:57 -0700

    cylinder added to shapes.js

--- src/shapes.js
+++ src/shapes.js
@@ -10,7 +10,75 @@
  * Let’s call the resulting data structure a “proto-geometry” because it has
  * the beginnings of a geometry but nothing close to what three.js has (yet).
  */
-const icosahedron = () => {
+const cylinder = () => {
+    // The core icosahedron coordinates.
+    const X = 0.9
+    const Z = 0.636
+  
+    return {
+      vertices: [
+        [0.0,  X, 0.0], //1
+        [0.0,  X,  -X], //2
+        [-Z,   X,  -Z], //3
+        [-X,   X, 0.0], //4
+        [-Z,   X,   Z], //5
+        [0.0,  X,   X], //6
+        [Z,    X,   Z], //7
+        [X,    X, 0.0], //8
+        [Z,    X,  -Z], //9
+
+        [0.0, -X, 0.0], //10
+        [0.0, -X,  -X], //11
+        [-Z,  -X,  -Z], //12
+        [-X,  -X, 0.0], //13
+        [-Z,  -X,   Z], //14
+        [0.0, -X,   X], //15
+        [Z,   -X,   Z], //16
+        [X,   -X, 0.0], //17
+        [Z,   -X,  -Z], //18
+      ],
+  
+      facesByIndex: [
+        [1, 2, 3], //Top
+        [1, 3, 4],
+        [1, 4, 5],
+        [1, 5, 6],
+        [1, 6, 7],
+        [1, 7, 8],
+        [1, 8, 9],
+        [1, 9, 2],
+
+        [10, 11, 12], //Bottom
+        [10, 12, 13],
+        [10, 13, 14],
+        [10, 14, 15],
+        [10, 15, 16],
+        [10, 16, 17],
+        [10, 17, 18],
+        [10, 18, 11],
+
+        [15, 6,  7], //Middle
+        [15, 7, 16],
+        [16, 7,  8],
+        [16, 8, 17],
+        [17, 8,  9],
+        [17, 9, 18],
+        [18, 9,  2],
+        [18, 2, 11],
+
+        [11, 2,  3], 
+        [11, 3, 12],
+        [12, 3,  4],
+        [12, 4, 13],
+        [13, 4,  5],
+        [13, 5, 14],
+        [14, 5,  6],
+        [14, 6, 15],
+      ]
+    }
+  }
+
+  const icosahedron = () => {
     // The core icosahedron coordinates.
     const X = 0.525731112119133606
     const Z = 0.850650808352039932
@@ -95,5 +163,5 @@ const icosahedron = () => {
     return result
   }
   
-  export { icosahedron, toRawTriangleArray, toRawLineArray }
+  export { cylinder, icosahedron, toRawTriangleArray, toRawLineArray }
   
