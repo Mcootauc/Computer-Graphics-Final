9cb8229b790dce360de1ada9d2019d9c57d9eaa7
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-03-28 18:32:44 -0700

    added updated pitched scene and scene.js

--- src/PitchedScene.js
+++ src/PitchedScene.js
@@ -4,32 +4,9 @@
  * a different group, many many weeks ago!
  */
 import { useEffect, useRef } from 'react'
-import { sphere, cone, cylinder, toRawLineArray } from './shapes'
-import { getGL, initVertexBuffer, initSimpleShaderProgram } from './glsl-utilities'
-
-const VERTEX_SHADER = `
-  #ifdef GL_ES
-  precision highp float;
-  #endif
-
-  attribute vec3 vertexPosition;
-
-  void main(void) {
-    gl_Position = vec4(vertexPosition, 1.0);
-  }
-`
-
-const FRAGMENT_SHADER = `
-  #ifdef GL_ES
-  precision highp float;
-  #endif
-
-  uniform vec3 color;
-
-  void main(void) {
-    gl_FragColor = vec4(color, 1.0);
-  }
-`
+import { cone, cylinder, toRawLineArray } from './shapes'
+import { getGL } from './glsl-utilities'
+import Scene from './scene/scene'
 
 const CANVAS_WIDTH = 512
 const CANVAS_HEIGHT = 512
@@ -41,12 +18,11 @@ const PitchedScene = props => {
   const canvasRef = useRef()
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas) {
+    const pitchCanvas = canvasRef.current
+    if (!pitchCanvas) {
       return
     }
-    // Grab the WebGL rendering context.
-    const gl = getGL(canvas)
+    const gl = getGL(pitchCanvas)
     if (!gl) {
       alert('No WebGL context found...sorry.')
 
@@ -54,18 +30,8 @@ const PitchedScene = props => {
       return
     }
 
-    // Set up settings that will not change.  This is not "canned" into a
-    // utility function because these settings really can vary from program
-    // to program.
-    gl.enable(gl.DEPTH_TEST)
-    gl.enable(gl.CULL_FACE)
-    gl.clearColor(0.0, 0.0, 0.0, 0.0)
-    gl.viewport(0, 0, canvas.width, canvas.height)
-
     // This variable stores 3D model information. We inline it for now but will want to separate it later.
-    // Think of these as proto-meshes, with no distinct geometry nor material.\
-    const obj =  toRawLineArray(cylinder())
-    console.log(obj + "cylinder")
+    // Think of these as proto-meshes, with no distinct geometry nor material.
     const objectsToDraw = [
 
       // Shape library demonstration.
@@ -78,81 +44,13 @@ const PitchedScene = props => {
         color: { r: 0.5, g: 1.0, b: 0 },
         vertices: toRawLineArray(cone()),
         mode: gl.LINES
-      },
-      {
-        color: { r: 0.0, g: 0.0, b: 0 },
-        vertices: toRawLineArray(sphere()),
-        mode: gl.LINES
       }
     ];
 
-    // Pass the vertices to WebGL.
-    objectsToDraw.forEach(objectToDraw => {
-      objectToDraw.verticesBuffer = initVertexBuffer(gl, objectToDraw.vertices)
-    })
+    //Sends canvas and objects to draw to the scene
+    Scene(pitchCanvas, objectsToDraw)
 
-    // Initialize the shaders.
-    let abort = false
-    const shaderProgram = initSimpleShaderProgram(
-      gl,
-      VERTEX_SHADER,
-      FRAGMENT_SHADER,
-
-      // Very cursory error-checking here...
-      shader => {
-        abort = true
-        alert('Shader problem: ' + gl.getShaderInfoLog(shader))
-      },
-
-      // Another simplistic error check: we don't even access the faulty
-      // shader program.
-      shaderProgram => {
-        abort = true
-        alert('Could not link shaders...sorry.')
-      }
-    )
-
-    // If the abort variable is true here, we can't continue.
-    if (abort) {
-      alert('Fatal errors encountered; we cannot continue.')
-      return
-    }
-
-    // All done --- tell WebGL to use the shader program from now on.
-    gl.useProgram(shaderProgram)
-
-    // Hold on to the important variables within the shaders.
-    const vertexPosition = gl.getAttribLocation(shaderProgram, 'vertexPosition')
-    gl.enableVertexAttribArray(vertexPosition)
-
-    /*
-     * Displays an individual object.
-     */
-    const drawObject = object => {
-      gl.uniform3f(gl.getUniformLocation(shaderProgram, 'color'), object.color.r, object.color.g, object.color.b)
-      gl.bindBuffer(gl.ARRAY_BUFFER, object.verticesBuffer)
-      gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0)
-      gl.drawArrays(object.mode, 0, object.vertices.length / 3)
-    }
-
-    /*
-     * Displays the scene.
-     */
-    const drawScene = () => {
-      // Clear the display.
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-      // Display the objects.
-      objectsToDraw.forEach(drawObject);
-    
-      // All done.
-      gl.flush();
-    };
-
-    // ...and finally, do the initial display.
-    drawScene()
-
-    const renderingContext = canvas.getContext('2d')
+    const renderingContext = pitchCanvas.getContext('2d')
     let previousTimestamp
     const nextFrame = timestamp => {
       // Initialize the timestamp.
